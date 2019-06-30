1f64a9323c2929fa45f8d2d2054b5e682309b5d2
Paulo Arruda <paulo.arruda85@gmail.com>
2019-06-29 21:14:32 -0300

    aula 13 finalizada

---
+++ aula13/ex001.js
@@ -0,0 +1,12 @@
+var count = 0;
+console.log('Agora vamos fazer o while.');
+while (count < 6) {
+    console.log(`Count ${count}`);
+    count++;
+}
+count = 0;
+console.log('Agora vamos fazer o do .. while.');
+do {
+    console.log(`Count ${count}`);
+    count++;
+} while (count < 6);
